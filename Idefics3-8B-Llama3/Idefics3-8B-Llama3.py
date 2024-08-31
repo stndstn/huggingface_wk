9a70614770ca2df097b9ae16dd55547c5632cb02
stndstn <takumi.tateishi@gmail.com>
2024-09-01 01:28:58 +0800

    update

--- Idefics3-8B-Llama3/Idefics3-8B-Llama3.py
+++ Idefics3-8B-Llama3/Idefics3-8B-Llama3.py
@@ -69,10 +69,23 @@ messages = [
         "content": [
             {"type": "image"},
             {"type": "text", "text": "This is Mayaisian driving license. What is the name and ID number of license holder?"},
+            #{"type": "text", "text": "List all lines of text in this image in json format."},          
+            #{"type": "text", "text": "List all lines of text in this image."},          
         ]
     },
 ]
+'''
+calling model.generate()...  23 : 27 : 34
+calling processor.batch_decode()...  23 : 35 : 2
+finished  23 : 35 : 2
+['User:<image>List all lines of text in this image in json format.\n
+Assistant: {\n    "id": "TAKUMI TATEISHI",\n    "nationality": "JPN",\n    "passport_number": "T2114505IJ",\n    "age": "B2 D",\n    "date_of_birth": "19/09/2016",\n    "expiry_date": "18/04/2021",\n    "address": "42-12F City Tower, Jln Alor Bkt Bintang, 50200 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur",\n    "photo": "A photo of a man"\n}']
 
+calling model.generate()...  23 : 39 : 37
+calling processor.batch_decode()...  0 : 0 : 3
+finished  0 : 0 : 3
+['User:<image>List all lines of text in this image.\nAssistant: ### Image Description\n\nThe image depicts a Malaysian driving license. The driving license is rectangular in shape and has a white background with a blue border. The top left corner features the emblem of Malaysia, which includes a crescent moon and a star, symbolizing the country\'s national identity. The text "LESEN MEMANDU" is written above the emblem, indicating that this is a driving license. Below the emblem, the word "MALAYSIA" is prominently displayed in blue capital letters, signifying the country of issuance.\n\nIn the center of the license, there is a photograph of a person, presumably the license holder. The person in the photo has short black hair and is wearing a white shirt. The photo is positioned slightly off-center to the left.\n\nBelow the photo, there is a section with personal information. The name "TAKUMI TATEISHI" is written in black capital letters. The text "JPN" is also present, which likely stands for "Jabatan Pendaftaran Negara," the National Registration Department of Malaysia. The license number "T2114505J" is displayed, followed by "B2 D," which indicates the type of license and possibly the class of vehicle it allows the holder to drive.\n\nThe validity of the license is specified as "19/09/2016 - 18/04/2021," indicating the start and end dates of the license\'s validity period. The address "42-12F CITY TOWER" is listed, followed by "JLN ALOR BINTANG," which is a street address in Kuala Lumpur, Malaysia. The postal code "50200" is also provided, which is the postal code for the area.\n\nThe bottom right corner of the license contains a small floral emblem, possibly a national symbol or a logo related to the issuing authority.\n\n### Analysis and Description\n\nThe driving license is a legal document issued by the Malaysian government to an individual named Takumi Tateishi. The license is valid for a specific period, from September 19, 2016, to April 18, 2021. The holder is allowed to drive a B2 class vehicle, which typically refers to a motorcycle with an engine capacity of up to 250cc. The address listed on the license is a commercial building in the heart of Kuala Lumpur, indicating that the holder may reside or work in this area.\n\nThe presence of the national emblem and the text "LESEN MEMANDU']
+'''
 now = time.localtime()
 print('calling processor.apply_chat_template... ', now.tm_hour, ':', now.tm_min, ':', now.tm_sec)
 
--- llava-1.5-7b-hf/llava-1.5-7b-hf_processor.py
+++ llava-1.5-7b-hf/llava-1.5-7b-hf_processor.py
@@ -49,12 +49,29 @@ conversation = [
           #{"type": "text", "text": "What is the title of this form?"},
           #{"type": "text", "text": "What is the text filled in the box labeled 'Name (Last, Suffix, First, Middle)' in 'SECTION A PERSONAL INFORMATION'?"},
           #{"type": "text", "text": "List all the information in 'SECTION A PERSONAL INFORMATION' of this form."},
-          {"type": "text", "text": "List all the text in this document."},
+          #{"type": "text", "text": "List all the text in this document."},
           #{"type": "text", "text": "List all the information in this image."},
+          #{"type": "text", "text": "This is Mayaisian driving license. What is the name and ID number of license holder?"},
+          {"type": "text", "text": "List all lines of text in this image in json format."},          
+          #{"type": "text", "text": "List all lines of text in this image."},          
           {"type": "image"},
         ],
     },
 ]
+'''
+[{'generated_text': "USER:  \n  List all the information in this document. \n\n
+  The image shows a Malaysian driver's license with a picture of a man on it. The license is pink and white and has a sticker on the back. The man's name is Takumu Tateishi, and his birth date is listed as 1947. The license is valid until 2016."}]
+[{'generated_text': 'USER:  \nThis is Mayaisian driving license. What is the name and ID number of license holder? \n\n
+  The name of the license holder is Takumu Tateishi. The ID number is 441119.'}]
+'''
+
+#calling pipe... 00:35:15
+#finish:  00:56:36
+#[{'generated_text': 'USER:  \nList all lines of text in this image. \n\n1. A man with a beard.\n2. A man with a beard.\n3. A man with a beard.\n4. A man with a beard.\n5. A man with a beard.\n6. A man with a beard.\n7. A man with a beard.\n8. A man with a beard.\n9. A man with a beard.\n10. A man with a beard.\n11. A man with a beard.\n12. A man with a beard.\n13. A man with a beard.\n14. A man with a beard.\n15. A man with a beard.\n16. A man with a beard.\n17. A man with a beard.\n18. A man with a beard.\n19. A man with a beard.\n20. A man with a beard.\n21. A man with a beard.\n22. A man with a beard.\n23. A man with a beard.\n24. A man with a beard.\n25. A man with a beard.\n26. A man with a beard.\n27. A man with a beard.\n28. A man with a beard.\n29. A man with a beard.\n30. A man with a beard.\n31. A man with a beard.\n32. A man with a beard.\n33. A man with a beard.\n34. A man with a beard.\n35. A man with a beard.\n36. A man with a beard.\n37. A man with a beard.\n38. A man with a beard.\n39. A man with a beard.\n40. A man with a beard.\n41. A man with a beard.\n42. A man with a beard.\n43. A man with a beard.\n44. A man with a beard.\n45. A man with a beard.\n46. A man with a beard.\n4'}]
+
+#calling pipe... 00:06:10
+#finish:  00:27:27
+#[{'generated_text': 'USER:  \nList all lines of text in this image in json format. \n{\n"image": {\n"filename": "takum\\_tatheishi\\_japanese\\_driving\\_license.jpg",\n"mime\\_type": "image/jpeg"\n},\n"license\\_number": "BZD D151421",\n"name": "Takum Tateishi",\n"nationality": "Japanese",\n"date\\_of\\_birth": "1974-09-19",\n"height": "170cm",\n"weight": "65kg",\n"eye\\_color": "Black",\n"hair\\_color": "Black",\n"hair\\_length": "Short",\n"hair\\_style": "Short",\n"facial\\_hair": "None",\n"skin\\_color": "Yellow",\n"complexion": "Fair",\n"ethnicity": "Asian",\n"gender": "Male",\n"occupation": "Student",\n"marital\\_status": "Single",\n"education": "High School",\n"national\\_id\\_number": "BZD D151421",\n"issuing\\_authority": "Japanese Government",\n"issuing\\_date": "2014-09-19",\n"expiry\\_date": "2016-09-19",\n"issuing\\_place": "Tokyo",\n"issuing\\_country": "Japan",\n"issuing\\_organization": "Japanese Government",\n"issuing\\_official": "Takum Tateishi",\n"issuing\\_official\\_title": "Student",\n"issuing\\_official\\_position": "Student",\n"issuing\\_official\\_department": "High School",\n"issuing\\_official\\_location": "Tokyo",\n"issuing\\_official\\_city": "Tokyo",\n"issuing\\_official\\_country": "Japan",\n"issuing\\_official\\_phone\\_number": "012-3456-7890",\n"issuing\\_official\\_email": "takum\\_tate'}]  
 
 print("calling processor.apply_chat_template...", time.strftime("%H:%M:%S", time.localtime()))
 # prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
